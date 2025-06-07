b929fb0fba592ad81855465c7e7c4c88f885e537
themelnykanton <themelnykanton@gmail.com>
2025-06-07 16:01:53 +0200

    Done

---
+++ js/task-2.js
@@ -0,0 +1,13 @@
+function formatMessage(message, maxLength) {
+  const result = message.length <= maxLength ? message : message.slice(0, maxLength) + '...'; 
+
+  return result;
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
